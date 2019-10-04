432260a742cf20e27d664dc4e1e14b3bf5f82995
mnottveit <mnottveit@gmail.com>
2019-10-04 16:36:46 +0200

    Fixed fsm and agent
    added password.txt

--- agent.py
+++ agent.py
@@ -1,32 +1,36 @@
 '''Plab 2 Gruppe 20'''
 
-from keypad import Keypad
-from ledBoard import Led_board
+# from keypad import Keypad
+# from ledBoard import Led_board
 
 
 class Agent:
     '''Main class Agent for controlling the hele driten'''
 
     def __init__(self):
-        self.keypad = Keypad()
-        self.led_board = Led_board()
+        # self.keypad = Keypad()
+        # self.led_board = Led_board()
         self.cump = ""
         self.cump_old = ""
         self.pass_path = "password.txt"
         self.override = ""
-        self.led = [0, 0] # [led_num, led_dur]
+        self.led = [0, 0]  # [led_num, led_dur]
+        self.digit = "0"
 
     def init_passcode_entry(self):
         '''Clear the password buffer (cump), and run startup light sequence'''
         self.cump = ""
         # TODO: self.led_board.startup()
+        print("startup")
 
     def get_next_signal(self):
         '''Return the override-signal if it is non-blank;
         otherwise query the keypad for the next pressed key.'''
         if self.override != "":
             return self.override
-        return self.keypad.get_next_signal()
+        # self.digit = self.keypad.get_next_signal()
+        self.digit = input("> ")
+        return self.digit
 
     def verify_login(self):
         # A3
@@ -35,38 +39,41 @@ class Agent:
         if self.cump == self.get_password():
             self.override = "Y"
             # TODO: self.led_board.correct_password()
-            self.cump = ""
-
-    def validate_passcode_change(self):
-        '''Check if new password is legal, maybe write to file, then run
-        light sequence'''
+            print("correct password")
+        else:
+            self.override = "N"
+        self.cump = ""
 
     def light_one_led(self):
         '''Light up the LED self.led[0] for self.led[1] seconds'''
+        print("Lighting LED", self.led[0], "for", self.led[1], "seconds")
 
     def flash_leds(self):
         '''Flash all LEDs'''
+        print("Flash all leds")
 
     def twinkle_leds(self):
         '''Twinkle all LEDs'''
+        print("Twinkle all leds")
 
     def exit_action(self):
         '''Initiate power down light sequence'''
+        print("exit")
 
     def get_password(self):
-        '''Returns the current password from the password-file'''
+        '''Returns the current password from the password file'''
         with open(self.pass_path, 'r') as file:
             password = file.readline().strip()
             return password
 
     def set_password(self, password):
-        '''a'''
+        '''Writes the new password to the password file'''
         with open(self.pass_path, 'w') as file:
             file.write(password)
 
-    def a2(self, digit):
+    def a2(self):
         """adds a digit to cump"""
-        self.cump += digit
+        self.cump += self.digit
 
     def a4(self):
         """Resets the FSM"""
@@ -77,6 +84,7 @@ class Agent:
 
     def a5(self):
         self.override = ""
+        self.cump = ""
 
     def a6(self):
         """Refreshes the FSM
@@ -95,6 +103,8 @@ class Agent:
         if self.cump == self.cump_old:
             self.override = "Y"
             # TODO: self.led_board.correct_password()
+        else:
+            self.override = "N"
 
     def a11(self):
         self.set_password(self.cump)
--- fsm.py
+++ fsm.py
@@ -1,11 +1,8 @@
-'''Plab 2 Gruppe 20'''
-
 from Oving_5.rule import Rule
 from Oving_5.agent import Agent
 
 
 class FSM:
-    '''FSM-class to control the finite state machine'''
     all = "#*0123456789N"
 
     def __init__(self, agent):
@@ -15,74 +12,78 @@ class FSM:
         self.run = True
         self.rule_list = []
         # From init
-        self.add_rule("init", "read", all, "A1")
+        print(FSM.all)
+        self.add_rule("init", "read", FSM.all, agent.init_passcode_entry)
         # From read
-        self.add_rule("read", "read", "0123456789", agent.a2())
-        self.add_rule("read", "verify", "*", agent.verify_login()) # a3()
-        self.add_rule("read", "init", all, agent.a4())
+        self.add_rule("read", "read", "0123456789", agent.a2)
+        self.add_rule("read", "verify", "*", agent.verify_login) # a3
+        self.add_rule("read", "init", FSM.all, agent.a4)
         # From verify
-        self.add_rule("verify", "active", "Y", agent.a5())
-        self.add_rule("verify", "init", all, agent.a4())
+        self.add_rule("verify", "active", "Y", agent.a5)
+        self.add_rule("verify", "init", FSM.all, agent.a4)
         # Logout process
-        self.add_rule("active", "logout", "#", agent.nothing())
-        self.add_rule("logout", "init", "#", agent.a4())
-        self.add_rule("logout", "active", all, agent.nothing())
+        self.add_rule("active", "logout", "#", agent.nothing)
+        self.add_rule("logout", "init", "#", agent.a4)
+        self.add_rule("logout", "active", FSM.all, agent.nothing)
         # LED process
         self.add_rule("active", "led", "012345", "A12")
-        self.add_rule("led", "time", "0123456789", agent.nothing())
-        self.add_rule("led", "active", all, agent.a6())
-        self.add_rule("time", "time", "0123456789", agent.a2())
+        self.add_rule("led", "time", "0123456789", agent.nothing)
+        self.add_rule("led", "active", FSM.all, agent.a6)
+        self.add_rule("time", "time", "0123456789", agent.a2)
         self.add_rule("time", "active", "*", "A10")
-        self.add_rule("time", "active", all, agent.a6())
+        self.add_rule("time", "active", FSM.all, agent.a6)
         # Password change process
         # Write password
-        self.add_rule("active", "newp", "*", agent.nothing())
-        self.add_rule("newp", "p1", "0123456789", agent.a2())
-        self.add_rule("newp", "active", all, agent.a6())
-        self.add_rule("p1", "p2", "0123456789", agent.a2())
-        self.add_rule("p1", "active", all, agent.a6())
-        self.add_rule("p2", "p3", "0123456789", agent.a2())
-        self.add_rule("p2", "active", all, agent.a6())
-        self.add_rule("p3", "p4", "0123456789", agent.a2())
-        self.add_rule("p3", "active", all, agent.a6())
-        self.add_rule("p4", "p4", "0123456789", agent.a2())
-        self.add_rule("p4", "p4", "*", agent.a7())
-        self.add_rule("p4", "active", all, agent.a6())
+        self.add_rule("active", "newp", "*", agent.nothing)
+        self.add_rule("active", "active", FSM.all, agent.nothing)
+        self.add_rule("newp", "p1", "0123456789", agent.a2)
+        self.add_rule("newp", "active", FSM.all, agent.a6)
+        self.add_rule("p1", "p2", "0123456789", agent.a2)
+        self.add_rule("p1", "active", FSM.all, agent.a6)
+        self.add_rule("p2", "p3", "0123456789", agent.a2)
+        self.add_rule("p2", "active", FSM.all, agent.a6)
+        self.add_rule("p3", "p4", "0123456789", agent.a2)
+        self.add_rule("p3", "active", FSM.all, agent.a6)
+        self.add_rule("p4", "p4", "0123456789", agent.a2)
+        self.add_rule("p4", "pc", "*", agent.a7)
+        self.add_rule("p4", "active", FSM.all, agent.a6)
         # Confirm password
-        self.add_rule("pc", "pc", "0123456789", agent.a2())
-        self.add_rule("pc", "verify2", "*", agent.a8())
-        self.add_rule("pc", "active", "#", agent.a6())
-        self.add_rule("verify2", "active", "Y", agent.a11())
-        self.add_rule("verify2", "active", all, agent.a6())
+        self.add_rule("pc", "pc", "0123456789", agent.a2)
+        self.add_rule("pc", "verify2", "*", agent.a8)
+        self.add_rule("pc", "active", "#", agent.a6)
+        self.add_rule("verify2", "active", "Y", agent.a11)
+        self.add_rule("verify2", "active", FSM.all, agent.a6)
 
     def add_rule(self, state1, state2, signal, action):
-        '''Adds a new rule to the end of self.rule_list'''
+        # Adds a new rule to the end of self.rule_list
         rule = Rule(state1, state2, signal, action)
         self.rule_list.append(rule)
 
     def get_next_signal(self):
-        '''Gets the next signal from the agent'''
+        # Gets the next signal from the agent
         self.current_signal = self.agent.get_next_signal()
 
     def run_rules(self):
-        '''Finds first satisfied rule and fires it'''
         for elem in self.rule_list:
             if self.apply_rule(elem):
                 self.fire_rule(elem)
                 break
 
     def apply_rule(self, rule):
-        '''Checks if the rule is satisfied'''
+        # Checks if the rule is satisfied
         return rule.check_rule(self.current_state, self.current_signal)
 
     def fire_rule(self, rule):
-        '''Changes the current_state, and excecutes the correct action.'''
+        # Changes the current_state, and excecutes the correct action.
         self.current_state = rule.state2
         rule.action()
 
 
-FSM = FSM(Agent())
+a = Agent()
+fsm = FSM(a)
 
-while FSM.run:
-    FSM.get_next_signal()
-    FSM.run_rules()
+while fsm.run:
+    print(fsm.current_state)
+    print(a.cump)
+    fsm.get_next_signal()
+    fsm.run_rules()
